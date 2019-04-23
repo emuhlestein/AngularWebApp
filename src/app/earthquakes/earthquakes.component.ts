3bec86a5aa3d0472bcc045b5b3b671f4a831c02c
Ed Muhlestein <emuhlestein@yahoo.com>
2019-04-23 17:27:26 -0500

    Enable paginator.

--- src/app/earthquakes/earthquakes.component.ts
+++ src/app/earthquakes/earthquakes.component.ts
@@ -14,6 +14,7 @@ import { timingSafeEqual } from 'crypto';
 })
 export class EarthquakesComponent implements OnInit {
 
+  pageSize: number = 16;
   earthquakes: Earthquake[];
   cols: any[];
   rowData: Earthquake;
